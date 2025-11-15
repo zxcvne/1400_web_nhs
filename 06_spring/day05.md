2212c32dbd138a7050edd4d99caed750c0fea89c
zxcvne <nhs8421@gmail.com>
2025-11-15 15:39:24 +0900

    update

---
+++ 06_spring/day05.md
@@ -0,0 +1,11 @@
+## Day5
+
+1. mapper
+
+   - 데이터베이스로부터 데이터를 조회/삭제/변경 등을
+     수행하는 기본적인 쿼리를 담당하는 메서드의 모음
+   - 공용 사용을 위해 비즈니스 로직은 거의 없음
+   - (주문 추가) insert order, (포인트 적립) insert point
+
+2. DAO(Data Access Object)
+   -
---
+++ 06_spring/dev/board/src/main/java/com/koreait/board/bean/dao/TimeDAO.java
@@ -0,0 +1,28 @@
+package com.koreait.board.bean.dao;
+
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.koreait.board.mapper.TimeMapper;
+
+@Repository // @Component의 자식
+public class TimeDAO {
+	@Autowired
+	private TimeMapper mapper;
+	
+	public String getTime() {
+		Random rd = new Random();
+		int num = rd.nextInt(3);
+		
+		if(num == 0) {
+			return mapper.getTime1();
+		}else if(num == 1) {
+			return mapper.getTime2();
+		}else {
+			return mapper.getTestString();
+		}
+	}
+	
+}
